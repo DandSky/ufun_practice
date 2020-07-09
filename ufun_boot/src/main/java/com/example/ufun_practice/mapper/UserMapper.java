76ec30a2aec9a7118ee41f6d3a5aeeb9d599bc3d
dandsky <runze1748@163.com>
2020-07-09 08:00:29 +0800

    this is version 2

--- ufun_boot/src/main/java/com/example/ufun_practice/mapper/UserMapper.java
+++ ufun_boot/src/main/java/com/example/ufun_practice/mapper/UserMapper.java
@@ -1,8 +1,12 @@
 package com.example.ufun_practice.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.ufun_practice.entity.User;
 
+import java.util.List;
+
 public interface UserMapper extends BaseMapper<User> {
 
+    List<User> selectOne(QueryWrapper<List<User>> queryWrapper);
 }
--- ufun_boot/src/test/java/com/example/ufun_practice/UserTest.java
+++ ufun_boot/src/test/java/com/example/ufun_practice/UserTest.java
@@ -1,9 +1,10 @@
 package com.example.ufun_practice;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.ufun_practice.entity.User;
 import com.example.ufun_practice.mapper.UserMapper;
-import net.minidev.json.JSONUtil;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,7 +22,7 @@ public class UserTest {
     @Autowired
     @Qualifier("userMapper")
     private UserMapper userMapper;
-
+/*
     @Test
     public void testSelect(){
         System.out.println("----- select All method test -----");
@@ -30,6 +32,43 @@ public class UserTest {
 
         System.out.println("------------Update----------");
         userMapper.update(userMapper, );
+    }*/
+
+
+    @Test
+    public void selectOne(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("name", "Jack");
+        User user = userMapper.selectOne(queryWrapper);
+        System.out.println(user);
+    }
+
+    @Test
+    public void selectLot(){
+    /*    List<User> count = userMapper.selectList(null);
+        for (User cou : count)
+            System.out.println(cou);
+*/
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.gt("age", 23);
+        List<User> resu = userMapper.selectList(queryWrapper);
+        for (User u : resu)
+            System.out.println(u);
+    }
+
+    @Test
+    public void selectMap(){
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.le("age", 24);
+        List<Map<String, Object>> map = userMapper.selectMaps(queryWrapper);
+    }
+
+    @Test
+    public void selectPage(){
+        Page<User> page = new Page<>(2, 5);
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        Page<User> userIPage = userMapper.selectPage(page, queryWrapper);
+        System.out.println(userIPage);
     }
 
 }
